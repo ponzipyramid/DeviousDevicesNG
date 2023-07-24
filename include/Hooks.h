6cadbdcb422114f70d4c31384005e35b9c85d72d
ponzipyramid <ponzipyramid123@proton.me>
2023-07-24 11:07:07 -0400

    fixed unequip hook

--- include/Hooks.h
+++ include/Hooks.h
@@ -3,24 +3,25 @@
 #include <detours/detours.h>
 
 namespace DeviousDevices {
-    typedef void(WINAPI* pFunc)(RE::ActorEquipManager a_1, RE::Actor* a_actor, RE::TESBoundObject* a_object, RE::ExtraDataList* a_extraData,
-                                std::uint32_t a_count, const RE::BGSEquipSlot* a_slot, bool a_queueEquip,
-                                bool a_forceEquip, bool a_playSounds, bool a_applyNow,
-                                const RE::BGSEquipSlot* a_slotToReplace);
+    typedef void(WINAPI* pFunc)(std::uint64_t a_1, RE::Actor* a_actor, RE::TESBoundObject* a_object,
+                                std::uint64_t a_extraData, std::uint64_t a_count, std::uint64_t a_slot,
+                                std::uint64_t a_queueEquip, std::uint64_t a_forceEquip, std::uint64_t a_playSounds,
+                                std::uint64_t a_applyNow, std::uint64_t a_slotToReplace);
     inline pFunc originalFunction;
     
-    inline void replacementFunction(RE::ActorEquipManager a_1, RE::Actor* a_actor, RE::TESBoundObject* a_object,
-                                    RE::ExtraDataList* a_extraData,
-                                    std::uint32_t a_count, const RE::BGSEquipSlot* a_slot, bool a_queueEquip,
-                                    bool a_forceEquip, bool a_playSounds, bool a_applyNow,
-                                    const RE::BGSEquipSlot* a_slotToReplace) {
-        SKSE::log::info("Test");
+    inline void replacementFunction(std::uint64_t a_1, RE::Actor* a_actor, RE::TESBoundObject* a_object,
+                                    std::uint64_t a_extraData, std::uint64_t a_count, std::uint64_t a_slot,
+                                    std::uint64_t a_queueEquip, std::uint64_t a_forceEquip,
+                                    std::uint64_t a_playSounds, std::uint64_t a_applyNow,
+                                    std::uint64_t a_slotToReplace) {
+
+        SKSE::log::info("{} unequipped {}", a_actor->GetFormID(), a_object->GetFormID());
 
         return originalFunction(a_1, a_actor, a_object, a_extraData, a_count, a_slot, a_queueEquip, a_forceEquip, a_playSounds,
                          a_applyNow, a_slotToReplace);
     }
 
-    void Install() {
+    inline void Install() {
         const auto targetAddress = RE::Offset::ActorEquipManager::UnequipObject.address();
         const auto funcAddress = &replacementFunction;
         originalFunction = (pFunc)targetAddress;
