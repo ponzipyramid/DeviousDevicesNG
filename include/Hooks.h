d0346713b447e4ab4fa594bde49f499b2edac42e
Andrej E Baranov <game@andb.name>
2024-04-28 05:02:13 +0700

    Disable magic/shout restrictions for VR
    not exist required addresses in VR address library.

--- include/Hooks.h
+++ include/Hooks.h
@@ -60,14 +60,9 @@ namespace DeviousDevices {
             static inline REL::Relocation<decltype(thunk)> func;
 
             static inline void Install() {
-                std::list targets{std::make_pair(RELOCATION_ID(37939, 38895), REL::VariantOffset(0x47, 0x47, 0x47))};
-
-                if (!REL::Module::IsVR()) {
-                    targets.push_back(
-                        std::make_pair(RELOCATION_ID(37950, 38906), REL::VariantOffset(0xC5, 0xCA, 0xC5)));
-                    targets.push_back(
-                        std::make_pair(RELOCATION_ID(37952, 38908), REL::VariantOffset(0xD7, 0xD7, 0xD7)));
-                }
+                std::array targets{std::make_pair(RELOCATION_ID(37939, 38895), REL::VariantOffset(0x47, 0x47, 0x47)),
+                                     std::make_pair(RELOCATION_ID(37950, 38906), REL::VariantOffset(0xC5, 0xCA, 0xC5)),
+                                     std::make_pair(RELOCATION_ID(37952, 38908), REL::VariantOffset(0xD7, 0xD7, 0xD7))};
 
                 auto& trampoline = SKSE::GetTrampoline();
 
@@ -96,12 +91,8 @@ namespace DeviousDevices {
             static inline REL::Relocation<decltype(thunk)> func;
 
             static inline void Install() {
-                std::list targets{std::make_pair(RELOCATION_ID(37941, 38897), REL::VariantOffset(0x21, 0x21, 0x21))};
-
-                if (!REL::Module::IsVR()) {
-                    targets.push_back(
-                        std::make_pair(RELOCATION_ID(37953, 38909), REL::VariantOffset(0x4B, 0x4B, 0x4B)));
-                }
+                std::array targets{std::make_pair(RELOCATION_ID(37941, 38897), REL::VariantOffset(0x21, 0x21, 0x21)),
+                                   std::make_pair(RELOCATION_ID(37953, 38909), REL::VariantOffset(0x4B, 0x4B, 0x4B))};
                 
                 auto& trampoline = SKSE::GetTrampoline();
 
@@ -180,10 +171,10 @@ namespace DeviousDevices {
 
             AddObjectToContainerHook::Install();
             PickUpObjectHook::Install();
-            if (ConfigManager::GetSingleton()->GetVariable<bool>("Hooks.bEquipSpell", true)) {
+            if (ConfigManager::GetSingleton()->GetVariable<bool>("Hooks.bEquipSpell", true) && !REL::Module::IsVR()) {
                 EquipSpellHook::Install();
             }
-            if (ConfigManager::GetSingleton()->GetVariable<bool>("Hooks.bEquipShout", true)) {
+            if (ConfigManager::GetSingleton()->GetVariable<bool>("Hooks.bEquipShout", true) && !REL::Module::IsVR()) {
                 EquipShoutHook::Install();
             }
 
