2a2f3d314468b9d933c95dd89636f41f6c5e4668
IHateMyKite <globglogabgalab991@gmail.com>
2024-08-08 09:34:57 +0200

    Fixed the force walk not restoring correctly

--- src/HooksVirtual.cpp
+++ src/HooksVirtual.cpp
@@ -16,16 +16,32 @@ void DeviousDevices::HooksVirtual::Setup()
 
 void DeviousDevices::HooksVirtual::ProcessButton(RE::MovementHandler* a_this, RE::ButtonEvent* a_event, RE::PlayerControlsData* a_data)
 {
-    ProcessButton_old(a_this,a_event,a_data);     
+    static bool loc_applied = false;
+
+    ProcessButton_old(a_this,a_event,a_data);
+
+    static bool loc_running = false;
+    if (!loc_applied)
+    {
+        loc_running = a_data->running;
+    }
 
     static auto loc_kwds = ConfigManager::GetSingleton()->GetArrayText("Movement.asForceWalkKeywords",false);
-
+    
     for (auto&& it :loc_kwds)
     {
         if (LibFunctions::GetSingleton()->WornHasKeyword(RE::PlayerCharacter::GetSingleton(),it))
         {
             a_data->running = false;
+            loc_applied = true;
             return;
         }
+    }
+
+    // restore original value
+    if (loc_applied)
+    {
+        loc_applied = false;
+        a_data->running = loc_running;
     }
 }
